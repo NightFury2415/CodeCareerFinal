eff56893e026f16eee9b8fdeba600f10c6f96eb0
MalavyaRaval <malavyaraval@gmail.com>
2025-08-23 12:08:48 -0700

    icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
